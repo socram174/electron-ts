d8345db950cc01f34e4157ba3997f15246b1a224
socram174 <socram174@hotmail.com>
2024-08-22 20:17:02 -0400

    chore: removed unused assets in the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
