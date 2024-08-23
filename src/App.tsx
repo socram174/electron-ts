064deacb9169290a800216880ec4ffbf1e469e88
socram174 <socram174@hotmail.com>
2024-08-22 20:14:10 -0400

    chore: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>CAMBIO FINAL ANTES DE SUBIR A GITHUB </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col justify-center items-center h-screen">
+          <h1 className="text-cyan-400 font-bold text-5xl">TAILWIND</h1>
+    </div>
   )
 }
 
